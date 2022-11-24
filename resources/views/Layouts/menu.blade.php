07cf77d342f79b0aaff6c8d2846fa616f1d1778a
Leguan122 <leguan25@gmail.com>
2022-11-24 12:47:16 +0100

    update

---
+++ resources/views/Layouts/menu.blade.php
@@ -0,0 +1,13 @@
+<div class="flex flex-row justify-between bg-black">
+    <div>
+        <img src="{{ url('Image/background.jpg') }}" class="h-24 object-contain">
+    </div>
+
+    <div >
+        <ul class="flex flex-row text-white h-full mr-3">
+            <li class="mx-2 flex items-center justify-center"><a href="#" class="">Referencie</a></li>
+            <li class="mx-2 flex items-center justify-center whitespace-nowrap"><a href="/aboutme" class="">O mne</a></li>
+            <li class="mx-2 flex items-center justify-center">Kontakt</li>
+        </ul>
+    </div>
+</div>
