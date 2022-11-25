45d39f670e26a596fcda1a043ec8518a18c0b982
Leguan122 <kiss.adam.bs@gmail.com>
2022-11-25 19:05:00 +0100

    aboutme section

--- resources/views/Layouts/menu.blade.php
+++ resources/views/Layouts/menu.blade.php
@@ -1,11 +1,38 @@
 <div class="flex flex-row justify-between bg-black">
     <div>
-        <img src="{{ url('Image/background.jpg') }}" class="h-24 object-contain">
+        <a href="/">
+            <img src="{{ url('Image/background.jpg') }}" class="h-24 ml-3 object-contain">
+        </a>
     </div>
 
     <div >
         <ul class="flex flex-row text-white h-full mr-3">
-            <li class="mx-2 flex items-center justify-center"><a href="#" class="">Referencie</a></li>
+{{--            FACEBOOK--}}
+            <li class="mx-2 flex items-center justify-center">
+                <a href="https://www.facebook.com/falconmilanjanos" target="_blank">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M13.355 22v-9.123h3.062l.459-3.555h-3.52v-2.27c0-1.03.285-1.731 1.761-1.731L17 5.32V2.14A25.233 25.233 0 0 0 14.257 2c-2.715 0-4.573 1.657-4.573 4.7v2.622h-3.07v3.555h3.07V22h3.671Z"/></svg>
+                </a>
+            </li>
+
+{{--            INSTAGRAM--}}
+            <li class="mx-2 flex items-center justify-center whitespace-nowrap">
+                <a href="https://www.instagram.com/falcon.video/?fbclid=IwAR30LWr717sS6JOAarFnOpNBYInG4FTQl2DV8ZbEhBZ4cAVIB2fRLNI3evQ" target="_blank">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3Z"/></svg>
+                </a>
+            </li>
+
+{{--            YOUTUBE--}}
+            <li class="mx-2 flex items-center justify-center">
+                <a href="https://www.youtube.com/channel/UCXom5i0sow5m7UvRb6HHvGg" target="_blank">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 48 48"><g fill="none" stroke="currentColor" stroke-linejoin="round"><path stroke-width="4" d="M11.064 10.414c5.543-.276 9.854-.414 12.934-.414s7.393.138 12.939.415a6 6 0 0 1 5.68 5.492c.254 3.034.381 5.706.381 8.017c0 2.339-.13 5.048-.39 8.128a6 6 0 0 1-5.587 5.483c-4.741.31-9.082.465-13.023.465c-3.94 0-8.28-.155-13.018-.465a6 6 0 0 1-5.587-5.48c-.263-3.103-.395-5.814-.395-8.131c0-2.29.129-4.963.385-8.02a6 6 0 0 1 5.68-5.49Z"/><path stroke-width="3.429" d="M21 19.61v8.796a.857.857 0 0 0 1.33.715l6.597-4.36a.857.857 0 0 0 .006-1.427l-6.598-4.436a.857.857 0 0 0-1.335.711Z"/></g></svg>
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    <div >
+        <ul class="flex flex-row text-white h-full mr-3">
+            <li class="mx-2 flex items-center justify-center"><a href="/" class="">Referencie</a></li>
             <li class="mx-2 flex items-center justify-center whitespace-nowrap"><a href="/aboutme" class="">O mne</a></li>
             <li class="mx-2 flex items-center justify-center">Kontakt</li>
         </ul>
--- resources/views/aboutme.blade.php
+++ resources/views/aboutme.blade.php
@@ -18,15 +18,29 @@
         </style>
     </head>
     <header>
-        <div class="w-full max-h-36">
-            <h1 class="text-center">Site under construction</h1>
+        <div class="w-full max-h-36 absolute">
+{{--            <h1 class="text-center">Site under construction</h1>--}}
             @include('Layouts.menu')
         </div>
     </header>
 
-    <body class="">
-        <div class="w-full min-h-screen max-h-screen bg-stone-900">
-            <iframe class="w-full h-3/6" ></iframe>
-        </div >
+    <body>
+
+    <div class="min-h-screen bg-stone-900 grid place-items-center">
+        <div class="max-w-md mx-auto bg-stone-800 rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+            <div class="md:flex">
+                <div class="md:shrink-0">
+                    <img class="h-48 w-full object-cover md:h-full md:w-48" src="https://scontent.fbts10-1.fna.fbcdn.net/v/t39.30808-6/243296131_4718820091475311_1175966634603051494_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=174925&_nc_ohc=QMFIPX2aHiwAX89Lgav&tn=JsM3Gb2Q2bcBYSG0&_nc_ht=scontent.fbts10-1.fna&oh=00_AfCd_BAVbf2k1lWO8W66isrMYYBmbAIvjp5GrpuQTIQCuw&oe=6385AC16" alt="Modern building architecture">
+                </div>
+                <div class="p-8">
+                    <div class="uppercase tracking-wide text-sm text-white font-semibold">O mne</div>
+                    <p class="mt-2 text-white">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquam nisi tellus, vitae facilisis erat sollicitudin vel. Duis id eleifend sem. Maecenas ut hendrerit augue. Fusce eu ex ut nulla vehicula scelerisque ut ac libero. Pellentesque congue vehicula lacus. Donec dolor nisi, lacinia eu velit commodo, fringilla sagittis est. Fusce posuere laoreet enim ac placerat. In pulvinar mollis massa sodales eleifend. In suscipit dolor diam, vel malesuada quam malesuada id. Mauris ut dolor ac sapien semper mollis. Suspendisse lacus purus, condimentum et libero sit amet, commodo suscipit tellus. Nunc consequat lacus ac augue dignissim, eu lacinia justo congue. Duis ac erat ipsum. Sed ut orci nulla.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
     </body>
 </html>
