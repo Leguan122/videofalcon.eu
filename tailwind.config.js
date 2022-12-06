b06203db9b60a4261eb30f6fe97f0019d210e069
Leguan122 <kiss.adam.bs@gmail.com>
2022-12-06 11:15:41 +0100

    detailing menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
       "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            'modern-black': '#181c21'
+        }
+    },
   },
   plugins: [],
 }
